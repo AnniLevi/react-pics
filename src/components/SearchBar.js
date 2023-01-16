27089e226429c2a34f669770c0b0439de7a12282
Anna Levitskaya <anna.levitskaya@polcode.net>
2023-01-16 18:08:48 +0100

    implement business logic

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,37 @@
-function SearchBar() {
-  return <div>SearchBar</div>;
+import { useState } from "react";
+
+function SearchBar({ onSubmit }) {
+  // const handleClick = () => {
+  //   onSubmit('cars');
+  // };
+
+  const [term, setTerm] = useState("");
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault(); // prevent the default form behavior (form input collection)
+
+    // NEVER DO THIS
+    // onSubmit(document.querySelector("input").value);
+
+    onSubmit(term);
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      {/* listen for button click */}
+      {/*<input />*/}
+      {/*<button onClick={handleClick}>Click me</button>*/}
+
+      {/*listen for Enter key press*/}
+      <form onSubmit={handleFormSubmit}>
+        <input value={term} onChange={handleChange} />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
