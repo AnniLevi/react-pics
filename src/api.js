33b81e78b41ed40704511dcada52b39c0330ca55
Anna Levitskaya <anna.levitskaya@polcode.net>
2023-01-16 18:44:24 +0100

    add environment variables

--- src/api.js
+++ src/api.js
@@ -2,10 +2,13 @@
 
 import axios from "axios";
 
+const apiUrl = process.env.REACT_APP_IMAGES_HOST_URL;
+const accessKey = process.env.REACT_APP_IMAGES_HOST_ACCESS_KEY;
+
 const searchImages = async (term) => {
-  const response = await axios.get("https://api.unsplash.com/search/photos", {
+  const response = await axios.get(apiUrl, {
     headers: {
-      Authorization: "Client-ID k81G9r4OcytbMOkC-K-hBJV6QZcVoBu4l9gehGBCRYc",
+      Authorization: `Client-ID ${accessKey}`,
     },
     params: {
       query: term,
