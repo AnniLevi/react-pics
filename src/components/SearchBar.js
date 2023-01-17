7d5688fae42820ed612510c66404403fd6e38aa3
Anna Levitskaya <anna.levitskaya@polcode.net>
2023-01-17 16:42:24 +0100

    add comments regarding handling text inputs

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -37,3 +37,22 @@ function SearchBar({ onSubmit }) {
 }
 
 export default SearchBar;
+
+/*
+Handling text inputs
+1. create a new piece of state
+2. create and event handler to watch for the 'onChange' event
+3. when the 'onChange' event fires, get the value from the input (event.target.value)
+4. take that value from the input and use it to update your state
+5. pass your state to the input as the value prop
+*/
+
+/*
+Why is it necessary?
+We steal control of the input value from the browser,
+and put the input value under the control of the State System.
+We can now manage the current value inside the inputs and change its value using the State System.
+- need to read the value of the input - look at the peace of state (here 'term')
+- need to update the value of the input - call setter (here 'setTerm')
+- component re-renders with every keypress - we can add more advanced features now
+ */
